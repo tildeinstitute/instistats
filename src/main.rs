d210c2ffe065431df381d2710d3958c4a5fe6a06
Ben Morrison <ben@gbmor.dev>
2019-09-07 14:22:00 -0400

    structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+const VERS: &str = "v0.1";
+const OUT_PATH: &str = "/var/www/htdocs/tilde.json";
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Server {
+    name: String,
+    url: String,
+    signup_url: String,
+    user_count: u32,
+    want_users: bool,
+    admin_email: String,
+    description: String,
+    users: Vec<User>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct User {
+    name: String,
+    title: String,
+    mtime: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("instistats {}", VERS);
+    println!("(c) 2019 Ben Morrison - ISC License");
+    println!();
+    println!("Path: {}", OUT_PATH);
+    println!();
 }
